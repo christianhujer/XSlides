e5a1b23692197fbb07e2667e83ce1c6cbb213b70
Christian Hujer <cher@riedquat.de>
2013-09-23 23:46:29 +0530

    Update XSlides.

--- slides.js
+++ slides.js
@@ -1,6 +1,72 @@
+NORMAL = 1;
+BLANK = 2;
+BLACK = 3;
+WHITE = 4;
+HELP = 5;
 currentSlide = 1;
-blank = false;
-commandlineEnable = false;
+blank = NORMAL;
+commandlineEnabled = false;
+nowheel = false;
+
+/** Event handler for mouse wheel.
+ * @param e Event object.
+ */
+function mousewheel(e) {
+    if (!e) { e = window.event; }
+    if (nowheel) {
+        nowheel = false;
+        return;
+    }
+    if (e.wheelDeltaY) {
+        /* Chrome supports two wheels, nice. */
+        if (e.wheelDeltaY > 0) { previousSlide(); }
+        if (0 > e.wheelDeltaY) { nextSlide(); }
+        return;
+    }
+    /* Opera and Internet Explorer support one wheel. */
+    if (e.wheelDelta > 0) { previousSlide(); }
+    if (0 > e.wheelDelta) { nextSlide(); }
+    /* No wheel support in Firefox. */
+}
+
+/** Eventhandler for wheel on div.
+ * @param e Event object.
+ */
+function divwheel(e) {
+    if (!e) { e = window.event; }
+    if (0 > e.wheelDelta) {
+        if (e.currentTarget.scrollHeight > e.currentTarget.scrollTop + e.currentTarget.clientHeight) {
+            nowheel = true;
+        }
+    }
+    if (e.wheelDelta > 0) {
+        if (e.currentTarget.scrollTop > 0) {
+            nowheel = true;
+        }
+    }
+}
+
+/** Event handler for mouse down event.
+ * @param e Event object.
+ */
+function mousedown(e) {
+    if (!e) { e = window.event; }
+    /*window.alert(e.button);*/
+    return false;
+}
+
+/** Event handler for mouse click event.
+ * @param e Event object.
+ */
+function click(e) {
+    if (!e) { e = window.event; }
+    /* Note: On IE, e.button is always 0 here - looks like a bug. */
+    switch (e.button) {
+    /*LMB*/case 0: nextSlide(); return false;
+    /*MMB*/case 1: previousSlide(); return false;
+    /*RMB*/case 2: /* TODO:2011-07-02:hujerc:3:Implement context menu. */; return false;
+    }
+}
 
 /** Event handler for the keydown event.
  * @param e Event object.
@@ -13,18 +79,19 @@ function keydown(e) {
     if (commandlineEnabled) {
         switch (keyCode) {
         case 27: disableCommandline(); return; /* esc */
-        /*default: window.alert(keyCode); */
+        /*default: window.alert(keyCode);*/
         }
     } else {
         switch (keyCode) {
+        case 8: previousSlide(); return false; /* Backspace */
         case 33: previousSlide(); return; /* PowerPoint: page up */
         case 34: nextSlide(); return; /* PowerPoint: page down */
         case 35: lastSlide(); return; /* PowerPoint: end */
         case 36: firstSlide(); return; /* PowerPoint: pos1 */
-        case 37: previousSlide(); return; /* PowerPoint: left */
-        case 38: previousSlide(); return; /* PowerPoint: up */
-        case 39: nextSlide(); return; /* PowerPoint: right */
-        case 40: nextSlide(); return; /* PowerPoint: down */
+        case 37: if (!commandlineEnabled) previousSlide(); return; /* PowerPoint: left */
+        case 38: if (!commandlineEnabled) previousSlide(); return; /* PowerPoint: up */
+        case 39: if (!commandlineEnabled) nextSlide(); return; /* PowerPoint: right */
+        case 40: if (!commandlineEnabled) nextSlide(); return; /* PowerPoint: down */
         case 112: help(); return false; /* PowerPoint: vi: F1 */
         case 116: enableCommandline(); return false; /* F5 */
         /*default: window.alert(keyCode);*/
@@ -33,14 +100,28 @@ function keydown(e) {
 }
 
 /** Toggles blanking the slides / screen. */
-function toggleBlank() {
-    if (blank) {
+function toggleBlank(newBlank) {
+    document.getElementById('screenWhite').style.display = 'none';
+    document.getElementById('screenBlack').style.display = 'none';
+    document.getElementById('slide_help').style.display = 'none';
+    if (newBlank == blank) {
+        blank = NORMAL;
         activateSlide(currentSlide);
-    } else {
-        document.getElementById('slide_' + currentSLide).style.display = 'none';
-        document.getElementById('currentSlide').innerHTML = currentSlide + ' (blank)';
+        return;
     }
-    blank = !blank;
+    if (newBlank == BLACK) {
+        document.getElementById('screenBlack').style.display = 'block';
+    } else if (newBlank == WHITE) {
+        document.getElementById('screenWhite').style.display = 'block';
+    } else if (newBlank == BLANK) {
+        document.getElementById('slide_' + currentSlide).style.display = 'none';
+        document.getElementById('currentSlide').innerHTML = currentSlide + ' (blank)';
+    } else if (newBlank == HELP) {
+        document.getElementById('slide_' + currentSlide).style.display = 'none';
+        document.getElementById('slide_help').style.display = 'block';
+        document.getElementById('currentSlide').innerHTML = currentSlide + ' (help)';
+    }
+    blank = newBlank;
 }
 
 /** Event handler for the keypress event.
@@ -70,7 +151,9 @@ function keypress(e) {
         case 71: lastSlide(); return; /* vi: G */
         case 103: firstSlide(); return; /* vi: g */
         case 58: retval = commandlineEnabled; enableCommandline(); return retval; /* vi: : */
-        case 46: if (!commandlineEnabled) toggleBlank(); return; /* PowerPoint: . */
+        case 46: if (!commandlineEnabled) toggleBlank(BLANK); return; /* PowerPoint: . */
+        case 98: if (!commandlineEnabled) toggleBlank(BLACK); return; /* PowerPoint: b */
+        case 119: if (!commandlineEnabled) toggleBlank(WHITE); return; /* PowerPoint: w */
         /*default: window.alert(keyCode);*/
         }
     }
@@ -81,7 +164,7 @@ function disableCommandline() {
     document.getElementById('command').blur();
     document.getElementById('commandline').style.display = 'none';
     document.getElementById('command').value = "";
-    comamndlineEnabled = false;
+    commandlineEnabled = false;
 }
 
 /** Enables the commandline. */
@@ -95,6 +178,9 @@ function enableCommandline() {
  * @param newSlide Slide to activate (number).
  */
 function activateSlide(newSlide) {
+    if (blank != NORMAL) {
+        return;
+    }
     if (newSlide > LAST_SLIDE) {
         newSlide = LAST_SLIDE;
     }
@@ -108,7 +194,7 @@ function activateSlide(newSlide) {
     if (document.getElementById('slide_' + currentSlide + "_logo")) {
         document.getElementById('logo').src = document.getElementById('slide_' + currentSlide + "_logo").src;
     } else {
-        docuemnt.getElementById('logo').src = logoSrc;
+        document.getElementById('logo').src = logoSrc;
     }
 }
 
@@ -136,19 +222,19 @@ function previousSlide() {
 function processCommand() {
     command = document.getElementById('command').value;
     command = command.replace(/^:/, "");
-    if (command.match(^\d+$)) {
+    if (command.match(/^\d+$/)) {
         activateSlide(Number(command));
     } else if (command.match(/^(\+|-)\d+$/)) {
         activateSlide(currentSlide + Number(command));
     } else if (command.match(/^e$/)) {
         window.location.reload();
-    } else if (comamnd.match(/^n(ext)?$/)) {
+    } else if (command.match(/^n(ext)?$/)) {
         nextSlide();
     } else if (command.match(/^prev(ious)?$/)) {
         previousSlide();
-    } else if (comamnd.match(/^fir(st)?$/)) {
+    } else if (command.match(/^fir(st)?$/)) {
         firstSlide();
-    } else if (comamnd.match(/^la(st)?$/)) {
+    } else if (command.match(/^la(st)?$/)) {
         lastSlide();
     }
     disableCommandline();
@@ -156,13 +242,31 @@ function processCommand() {
 
 /** Display help. */
 function help() {
-    /* TODO:2011-03-30:cher:3:Implement help. */
+    toggleBlank(HELP);
 }
 
 /** Start handler. */
 function begin() {
     logoSrc = document.getElementById('logo').src;
     activateSlide(1);
+    divs = document.getElementsByTagName('div');
+    for (i = 0; divs.length > i; i++) {
+        el = divs.item(i);
+        el.onmousewheel = divwheel;
+    }
+}
+function enableNavigator() {
+    /* TODO:2011-07-02:hujerc:3:Implement enable navigator. */
+    document.body.style.cursor = 'crosshair';
+    /*document.getElementById('navBar').style.display = 'block';*/
+}
+function disableNavigator() {
+    /* TODO:2011-07-02:hujerc:3:Implement disable navigator. */
+    document.body.style.cursor = 'default';
+    /*document.getElementById('navBar').style.display = 'none';*/
 }
 document.onkeydown = keydown;
 document.onkeypress = keypress;
+document.onclick = click;
+document.onmousedown = mousedown;
+document.onmousewheel = mousewheel;
--- slides.xslt
+++ slides.xslt
@@ -8,6 +8,8 @@
     xmlns:svg="http://www.w3.org/2000/svg"
 >
 
+    <xsl:param name="imgmode" />
+
     <xsl:preserve-space elements="html:* *" />
 
     <xsl:output
@@ -19,19 +21,95 @@
         doctype-system="http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg.dtd"
     -->
 
-    <xsl:template match="slides:slides">
-        <html xml:lang="en">
-            <head>
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title><xsl:apply-templates select="slides:title//text()" /></title>
-                <style type="text/css"><xsl:copy-of select="document('slides.css.xml')/css/node()"/></style>
-                <script type="text/ecmascript">LAST_SLIDE = <xsl:value-of select="count(slides:slide)" /></script>
-                <script type="text/ecmascript"><xsl:copy-of select="document('slides.js.xml')/js/node()"/></script>
-            </head>
-            <body>
-                <xsl:apply-templates select="slides:slide" />
-            </body>
-        </html>
+    <xsl:template match="/slides:slides">
+<html xml:lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title><xsl:apply-templates select="slides:title//text()" /></title>
+        <style type="text/css"><xsl:copy-of select="document('slides.css.xml')/css/node()"/></style>
+        <script type="text/ecmascript">LAST_SLIDE = <xsl:value-of select="count(slides:slide)" /></script>
+        <script type="text/ecmascript"><xsl:copy-of select="document('slides.js.xml')/js/node()"/></script>
+    </head>
+    <body onload="javascript:begin();" onmouseover="javascript:enableNavigator();">
+        <div class="slide" id="slide_help">
+            <div class="headerregion">
+                <div class="header">
+                    <h1>Help</h1>
+                </div>
+            </div>
+            <div class="bodyregion">
+                <div class="full">
+                    <p>
+                        Welcome to XSlides.
+                    </p>
+                    <h2>Key Strokes</h2>
+                    <p>Design Notes:</p>
+                    <ul>
+                        <li>Key Strokes applicable from PowerPoint and from VI Improved work as expected.</li>
+                        <li>Your Logitech Cordless Presenter and similar tools work, too.</li>
+                    </ul>
+                    <h3>Normale KeyStrokes</h3>
+                    <table class="lnice">
+                        <tr><th>Function</th><th>Keys</th></tr>
+                        <tr><td>Help</td><td>F1</td></tr>
+                        <tr><td>Next Slide</td><td>Page Down, Cursor Down, Cursor Right, j, l, n, Return, Enter, Space, Mouse Button 1 (Left Mouse Button)</td></tr>
+                        <tr><td>Previous Slide</td><td>Page Up, Cursor Left, Cursor Up, h, k, p, Backspace</td></tr>
+                        <tr><td>First Slide</td><td>Pos1, g</td></tr>
+                        <tr><td>Last Slide</td><td>End, G</td></tr>
+                        <tr><td>Blank Slide</td><td>.</td></tr>
+                        <tr><td>Black Screen</td><td>b</td></tr>
+                        <tr><td>White Screen</td><td>w</td></tr>
+                        <tr><td>Reload</td><td>Press F5 twice</td></tr>
+                        <tr><td>Enable Command Line</td><td>F5, :</td></tr>
+                    </table>
+                    <h3>Commands to type in Command Line</h3>
+                    <table class="lnice">
+                        <tr><th>Command</th><th>Function</th></tr>
+                        <tr><td>Number (i.e. 15)</td><td>Goto the slide with that number (i.e. goto slide 15).</td></tr>
+                        <tr><td>+Number (i.e. +3)</td><td>Go number slides forward (i.e. go 3 slides forward).</td></tr>
+                        <tr><td>-Number (i.e. -3)</td><td>Go number slides backward (i.e. go 3 slides backward).</td></tr>
+                        <tr><td>n, next</td><td>Next Slide</td></tr>
+                        <tr><td>prev, previous</td><td>Previous Slide</td></tr>
+                        <tr><td>fir, first</td><td>Goto first slide</td></tr>
+                        <tr><td>la, last</td><td>Goto last slide</td></tr>
+                        <tr><td>e, F5</td><td>Reload</td></tr>
+                    </table>
+                    <h2>Mouse Wheel Support</h2>
+                    <ul>
+                        <li>In Opera, the Mouse Wheel works perfect. If the wheel can scroll, it scrolls, otherwise it changes the current slide.</li>
+                        <li>In Chrome, the wheel works near perfect. It scrolls / changes slides as expected, but a new slide always has divs scrolled to top instead of remembering the last scroll position.</li>
+                        <li>In Internet Explorer, the wheel only changes slides.</li>
+                        <li>In Firefox, the wheel doesn't work at all.</li>
+                    </ul>
+                    <h2>Tested Versions</h2>
+                    <ul>
+                        <li>Google Chrome 12.0.742.100</li>
+                        <li>Microsoft Internet Explorer 8.0.6001.18702</li>
+                        <li>Mozilla Firefox 4.0</li>
+                        <li>Opera 11.10 Build 2092</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <xsl:apply-templates select="slides:slide" />
+        <div class="footerregion">
+            <div class="footer">
+                <span style="font-size:smaller;">
+                    <span id="currentSlide">1</span>/<xsl:value-of select="count(html:slide)" />
+                    <br />
+                    <xsl:text>Made with XSlides. XSlides is © 2011 Christian Hujer.</xsl:text>
+                </span>
+            </div>
+        </div>
+        <div id="commandline">
+            <form method="get" action="javascript:processCommand();">
+                <label for="command">:</label><input type="text" id="command" />
+            </form>
+        </div>
+        <div id="screenWhite"><xsl:text> </xsl:text></div>
+        <div id="screenBlack"><xsl:text> </xsl:text></div>
+    </body>
+</html>
     </xsl:template>
 
     <xsl:template match="slides:slide">
@@ -51,6 +129,39 @@
 
     <xsl:template match="slides:listing">
         <xsl:apply-templates select="document(concat(@src, '.listing'))/html:body/html:pre" />
+    </xsl:template>
+
+    <xsl:template match="html:img[contains(@src, '.svg')]">
+        <xsl:apply-templates select="document(@src, .)" />
+    </xsl:template>
+
+    <xsl:template match="html:img/@src">
+        <xsl:attribute name="src">
+            <xsl:choose>
+                <xsl:when test="$imgmode='base64' and document(concat(., '.b64'), .)/base64">
+                    <xsl:text>data:image/png;base64,</xsl:text>
+                    <xsl:value-of select="document(concat(., '.b64'), .)/base64" />
+                </xsl:when>
+                <xsl:otherwise>
+                    <xsl:value-of select="." />
+                </xsl:otherwise>
+            </xsl:choose>
+        </xsl:attribute>
+    </xsl:template>
+
+    <xsl:template name="imgSrc">
+        <xsl:param name="src" />
+        <xsl:attribute name="src">
+            <xsl:choose>
+                <xsl:when test="$imgmode='base64' and document(concat($src, '.b64'), .)/base64">
+                    <xsl:text>data:image/png;base64,</xsl:text>
+                    <xsl:value-of select="document(concat($src, '.b64'), .)/base64" />
+                </xsl:when>
+                <xsl:otherwise>
+                    <xsl:value-of select="$src" />
+                </xsl:otherwise>
+            </xsl:choose>
+        </xsl:attribute>
     </xsl:template>
 
     <xsl:template match="@*|node()">
