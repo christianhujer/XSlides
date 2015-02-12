a12bbbb9148d8565a7be1c26c2fc1b3f42c694c6
Christian Hujer <cher@riedquat.de>
2015-02-12 19:27:41 +0530

    Cleanup code.

--- XSlides.js
+++ XSlides.js
@@ -1,190 +1,195 @@
-var XSlides_currentSlide = 0;
-var XSlides_numberOfSlides = 0;
-var commandlineEnabled = false;
-
-function XSlides_displaySlide(slideNumber) {
-
-    function getSlideId(element) {
-        var childNodes = element.childNodes;
-        for (var i = 0; i < childNodes.length; i++) {
-            var childNode = childNodes[i];
-            if (childNode.id)
-                return childNode.id;
-            if (element.childNodes)
-                return getSlideId(childNode);
-        }
-    }
-
-    if (1 > slideNumber)
-        slideNumber = 1;
-    if (slideNumber > XSlides_numberOfSlides)
-        slideNumber = XSlides_numberOfSlides;
-    if (slideNumber == XSlides_currentSlide)
-        return;
-    if (XSlides_currentSlide != 0)
-        document.getElementById('slide' + XSlides_currentSlide).style.display = 'none';
-    XSlides_currentSlide = slideNumber;
-    var currentSlide = document.getElementById('slide' + XSlides_currentSlide);
-    currentSlide.style.display = 'block';
-    var slideId = getSlideId(currentSlide);
-    if (!slideId)
-        slideId = '(' + XSlides_currentSlide + ')';
-    window.location.hash = '#' + slideId;
-}
-
-function XSlides_onload() {
-    var slideNumber = 0;
-
-    function isSlideStart(element) {
-        return element.tagName == "h1" || element.tagName == "H1";
-    }
-
-    function isSlideEnd(element) {
-        var nextSibling = element.nextSibling;
-        return !nextSibling || isSlideStart(nextSibling);
-    }
-
-    function createSlideDiv(childNodes) {
-        var divElement = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
-        divElement.setAttribute('class', 'slide');
-        divElement.setAttribute('id', 'slide' + ++slideNumber);
-        for (var i = 0; i < childNodes.length; ++i)
-            divElement.appendChild(childNodes[i]);
-        divElement.slideNumber = slideNumber;
-        return divElement;
-    }
-
-    function convertHeadingsIntoSlides() {
-        var nodes = document.body.childNodes;
-        var newElements = [];
-        var elementsOfCurrentSlide = [];
-        var slideStartFound = false;
-
+var Util = {
+    getFirstDescendantId : function(node) {
+        var nodes = node.childNodes;
         for (var i = 0; i < nodes.length; i++) {
-            var currentElement = nodes.item(i);
-            elementsOfCurrentSlide.push(currentElement);
-            if (isSlideStart(currentElement))
-                slideStartFound = true;
-            if (slideStartFound && isSlideEnd(currentElement)) {
-                XSlides_numberOfSlides++;
-                newElements.push(createSlideDiv(elementsOfCurrentSlide));
-                elementsOfCurrentSlide = [];
-                i = -1;
-                slideStartFound = false;
-            }
+            var node = nodes[i];
+            if (node.id)
+                return node.id;
+            if (node.childNodes)
+                return Util.getFirstDescendantId(node);
         }
-        for (var i = 0; i < newElements.length; i++)
-            document.body.appendChild(newElements[i]);
-    }
-
-    function previousSlide() {
-        XSlides_displaySlide(XSlides_currentSlide - 1);
-    }
-
-    function nextSlide() {
-        XSlides_displaySlide(XSlides_currentSlide + 1);
-    }
-
-    function firstSlide() {
-        XSlides_displaySlide(1);
-    }
-
-    function lastSlide() {
-        XSlides_displaySlide(XSlides_numberOfSlides);
-    }
-
-    function keydown(e) {
-        if (!e) e = window.event;
-        var keyCode = e.keyCode;
-        switch (keyCode) {
-        case 8: previousSlide(); return false; /* Backspace */
-        case 33: previousSlide(); return; /* PowerPoint: page up */
-        case 34: nextSlide(); return; /* PowerPoint: page down */
-        case 35: lastSlide(); return; /* PowerPoint: end */
-        case 36: firstSlide(); return; /* PowerPoint: pos1 */
-        case 37: if (!commandlineEnabled) previousSlide(); return; /* PowerPoint: left */
-        case 38: if (!commandlineEnabled) previousSlide(); return; /* PowerPoint: up */
-        case 39: if (!commandlineEnabled) nextSlide(); return; /* PowerPoint: right */
-        case 40: if (!commandlineEnabled) nextSlide(); return; /* PowerPoint: down */
-        }
-    }
-
-    function keypress(e) {
-        if (!e) e = window.event;
-        var keyCode = e.keyCode;
-        switch (keyCode) {
-        case 104: previousSlide(); return; /* vi: h */
-        case 106: nextSlide(); return; /* vi: j */
-        case 107: previousSlide(); return; /* vi: k */
-        case 108: nextSlide(); return; /* vi: l */
-        case 110: nextSlide(); return; /* PowerPoint: n */
-        case 112: previousSlide(); return; /* PowerPoint: p */
-        case 32: nextSlide(); return; /* PowerPoint: <SP> */
-        case 49: firstSlide(); return; /* vi: 1 */
-        case 13: nextSlide(); return; /* PowerPoint: <CR> */
-        case 10: nextSlide(); return; /* PowerPoint: <LF> */
-        case 71: lastSlide(); return; /* vi: G */
-        case 103: firstSlide(); return; /* vi: g */
-        }
-    }
-
-    function onhashchange() {
-        displaySlideFromHash();
-    }
-
-    function installEventHandlers() {
-        document.onkeydown = keydown;
-        document.onkeypress = keypress;
-        window.onhashchange = onhashchange;
-    }
-
-    function addXSlidesStylesheet() {
-        linkStylesheet('XSlides.css');
-
-        var stylename = getSearchParameter('style');
-        if (stylename)
-            linkStylesheet('styles/' + stylename + '.css');
-    }
-
-    function linkStylesheet(href) {
-        var linkEl = document.createElementNS('http://www.w3.org/1999/xhtml', 'link');
-        linkEl.setAttribute('rel', 'Stylesheet');
-        linkEl.setAttribute('type', 'text/css');
-        linkEl.setAttribute('href', href);
-        document.head.appendChild(linkEl);
-    }
-
-    function getSearchParameter(name) {
+    },
+    getSearchParameter : function(name) {
         var search = window.location.search.substring(1).split("&");
         for (var i = 0; i < search.length; i++) {
             var kv = search[i].split("=");
             if (kv[0] == name)
                 return kv[1];
         }
-    }
+    },
+};
 
-    function getSlideNumberFromHash(hash) {
+var XSlides = {
+    currentSlide : null,
+    numberOfSlides : 0,
+
+    /* Helper methods */
+    isFirstNodeOfSlide : function(node) {
+        return node.tagName == "h1" || node.tagName == "H1";
+    },
+
+    isLastNodeOfSlide : function(node) {
+        var nextSibling = node.nextSibling;
+        return !nextSibling || this.isFirstNodeOfSlide(nextSibling);
+    },
+
+    getSlideNumberFromHash : function(hash) {
         var match;
         if (match = /^#\((\d+)\)$/.exec(hash))
             return Number(match[1]);
         var id = hash.substring(1);
-       for (var element = document.getElementById(id); element; element = element.parentNode)
-           if (element.slideNumber)
-               return element.slideNumber;
-    }
+        for (var element = document.getElementById(id); element; element = element.parentNode)
+            if (element.slideNumber)
+                return element.slideNumber;
+    },
 
-    function displaySlideFromHash() {
+    /* private methods */
+    installEventHandlers : function() {
+        document.addEventListener('keydown', function() { XSlides.keydown() }, false);
+        document.addEventListener('keypress', function() { XSlides.keypress() }, false);
+        window.addEventListener('hashchange', function() { XSlides.hashchange() }, false);
+    },
+
+    addXSlidesStylesheet : function() {
+        this.linkStylesheet('XSlides.css');
+
+        var stylename = Util.getSearchParameter('style');
+        if (stylename)
+            this.linkStylesheet('styles/' + stylename + '.css');
+    },
+
+    linkStylesheet : function(href) {
+        var linkEl = document.createElementNS('http://www.w3.org/1999/xhtml', 'link');
+        linkEl.setAttribute('rel', 'Stylesheet');
+        linkEl.setAttribute('type', 'text/css');
+        linkEl.setAttribute('href', href);
+        document.head.appendChild(linkEl);
+    },
+
+    createSlideDiv : function(childNodes) {
+        var divElement = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+        divElement.setAttribute('class', 'slide');
+        divElement.setAttribute('id', 'slide' + ++XSlides.numberOfSlides);
+        for (var i = 0; i < childNodes.length; ++i)
+            divElement.appendChild(childNodes[i]);
+        divElement.slideNumber = XSlides.numberOfSlides;
+        return divElement;
+    },
+
+    convertHeadingsIntoSlides : function() {
+        var nodes = document.body.childNodes;
+        var slides = [];
+        var nodesOfCurrentSlide = [];
+        var slideStartFound = false;
+
+        for (var i = 0; i < nodes.length; i++) {
+            var currentNode = nodes.item(i);
+            nodesOfCurrentSlide.push(currentNode);
+            if (XSlides.isFirstNodeOfSlide(currentNode))
+                slideStartFound = true;
+            if (slideStartFound && XSlides.isLastNodeOfSlide(currentNode)) {
+                slides.push(XSlides.createSlideDiv(nodesOfCurrentSlide));
+                nodesOfCurrentSlide = [];
+                i = -1;
+                slideStartFound = false;
+            }
+        }
+        for (var i = 0; i < slides.length; i++)
+            document.body.appendChild(slides[i]);
+    },
+
+    /* public methods */
+    init : function() {
+        window.addEventListener('load', XSlides.load, false);
+    },
+
+    displaySlide : function(slideNumber) {
+        if (1 > slideNumber) slideNumber = 1;
+        if (slideNumber > this.numberOfSlides) slideNumber = this.numberOfSlides;
+        if (slideNumber == this.currentSlide) return;
+        if (this.currentSlide)
+            document.getElementById('slide' + this.currentSlide).style.display = 'none';
+        this.currentSlide = slideNumber;
+        var currentSlide = document.getElementById('slide' + this.currentSlide);
+        currentSlide.style.display = 'block';
+        var slideId = Util.getFirstDescendantId(currentSlide);
+        if (!slideId)
+            slideId = '(' + this.currentSlide + ')';
+        window.location.hash = '#' + slideId;
+    },
+
+    displaySlideFromHash : function() {
         var hash = window.location.hash;
         var slideNumber;
-        if (hash) slideNumber = getSlideNumberFromHash(hash);
+        if (hash) slideNumber = this.getSlideNumberFromHash(hash);
         if (!slideNumber) slideNumber = 1;
-        XSlides_displaySlide(slideNumber);
-    }
+        this.displaySlide(slideNumber);
+    },
 
-    addXSlidesStylesheet();
-    convertHeadingsIntoSlides();
-    installEventHandlers();
-    displaySlideFromHash();
-}
+    firstSlide : function() {
+        this.displaySlide(1);
+    },
 
-window.addEventListener('load', XSlides_onload, false);
+    previousSlide : function() {
+        this.displaySlide(this.currentSlide - 1);
+    },
+
+    nextSlide : function() {
+        this.displaySlide(this.currentSlide + 1);
+    },
+
+    lastSlide : function() {
+        this.displaySlide(this.numberOfSlides);
+    },
+
+    /* event handlers */
+    keydown : function(e) {
+        console.log(this);
+        if (!e) e = window.event;
+        var keyCode = e.keyCode;
+        switch (keyCode) {
+        case 8: this.previousSlide(); return false; /* Backspace */
+        case 33: this.previousSlide(); return; /* PowerPoint: page up */
+        case 34: this.nextSlide(); return; /* PowerPoint: page down */
+        case 35: this.lastSlide(); return; /* PowerPoint: end */
+        case 36: this.firstSlide(); return; /* PowerPoint: pos1 */
+        case 37: this.previousSlide(); return; /* PowerPoint: left */
+        case 38: this.previousSlide(); return; /* PowerPoint: up */
+        case 39: this.nextSlide(); return; /* PowerPoint: right */
+        case 40: this.nextSlide(); return; /* PowerPoint: down */
+        }
+    },
+
+    keypress : function(e) {
+        if (!e) e = window.event;
+        var keyCode = e.keyCode;
+        switch (keyCode) {
+        case 104: this.previousSlide(); return; /* vi: h */
+        case 106: this.nextSlide(); return; /* vi: j */
+        case 107: this.previousSlide(); return; /* vi: k */
+        case 108: this.nextSlide(); return; /* vi: l */
+        case 110: this.nextSlide(); return; /* PowerPoint: n */
+        case 112: this.previousSlide(); return; /* PowerPoint: p */
+        case 32: this.nextSlide(); return; /* PowerPoint: <SP> */
+        case 49: this.firstSlide(); return; /* vi: 1 */
+        case 13: this.nextSlide(); return; /* PowerPoint: <CR> */
+        case 10: this.nextSlide(); return; /* PowerPoint: <LF> */
+        case 71: this.lastSlide(); return; /* vi: G */
+        case 103: this.firstSlide(); return; /* vi: g */
+        }
+    },
+
+    hashchange : function() {
+        this.displaySlideFromHash();
+    },
+
+    load : function() {
+
+        XSlides.addXSlidesStylesheet();
+        XSlides.convertHeadingsIntoSlides();
+        XSlides.installEventHandlers();
+        XSlides.displaySlideFromHash();
+    },
+};
+
+XSlides.init();
